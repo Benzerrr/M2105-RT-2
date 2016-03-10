864978399e6cf18caeb005e13b06fe77248a8c34
benjamin.duval <21503856@etu.unicaen.fr>
2016-03-10 17:22:50 +0100

    commit 3

--- tp1/exercice 3.php
+++ tp1/exercice 3.php
@@ -5,3 +5,16 @@
  * Date: 10/03/2016
  * Time: 17:00
  */
+
+
+function createHTMLTable ($l,$c){
+    echo "<table border='1'>";
+    for ($i = 0; $i <$l; $i++) {
+        echo "<tr>";
+        for($j=0;$j<$c; $i++){
+            echo "<td>{$i} - {$j}</td>";
+        }
+        echo "</td>";
+    }
+    echo "</table>";
+}
