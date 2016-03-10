9be6e5c0a20034f140121f5bc7269de3e7349b80
benjamin.duval <21503856@etu.unicaen.fr>
2016-03-10 17:46:14 +0100

    bqdfklbklqetklklqsbtrksfhkl

---
+++ tp1/exercice 4.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin.duval
+ * Date: 10/03/2016
+ * Time: 17:27
+ */
+
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+    $rows=$_POST("rows");
+    $cols=$_POST("cols");
+
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+<form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+<label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+<label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+<input type=""submit" value="Créer le tableau">
+</form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
