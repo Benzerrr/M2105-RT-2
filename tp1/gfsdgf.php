10c5ee99738242eab5ddf7cf49886e5c6be5fac1
benjamin.duval <21503856@etu.unicaen.fr>
2016-03-10 17:51:05 +0100

    first commit

---
+++ tp1/gfsdgf.php
@@ -0,0 +1,576 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benjamin.duval
+ * Date: 10/03/2016
+ * Time: 17:50
+ */
+* Created by PhpStorm.
+ * User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+v
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
+
+* Created by PhpStorm.
+* User: benjamin.duval
+* Date: 10/03/2016
+* Time: 17:27
+*/
+function createHTMLTable ($nbLignes,$nbCols)(...)
+?>
+<!doctype html>
+<html>
+<meta charset=""utf-8">
+<head></head>
+<body>
+<?php
+$cols=0;$rows=0
+if(array_key_exists("rows",$_POST))(
+$rows=$_POST("rows");
+$cols=$_POST("cols");
+)
+?>
+<fieldset>
+    <legend>Création de tableau</legend>
+    <form method=""post" action="<?php echo $_SERVER("PHP_SELF"):?>">
+    <label>Lignes: <input type=""number" name="rows" id="rows" value="<?php echo $rows?>"></label>
+    <label>Colonnes: <input type=""number" name="Cols" id="cols" value="<?php echo $cols?>"></label>
+    <input type=""submit" value="Créer le tableau">
+    </form>
+</fieldset>
+<?php
+echo createHTMLTable($rows,$cols)
+?>
+</body>
+</html>
+
+Status API Training Shop Blog About Pricing
+
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+
